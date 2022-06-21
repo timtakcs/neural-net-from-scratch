ed42e818d31ec780f0a21fe11d3b0818c82d58cf
timtakcs <timur_takhtarov@houston.nae.school>
2022-06-21 15:22:51 -0500

    forward propagation works

--- Neural_net_from_scratch/Source.cpp
+++ Neural_net_from_scratch/Source.cpp
@@ -4,26 +4,57 @@
 using namespace std;
 using namespace Eigen;
 
-class Net {
-public:
-	Net() {
+float cross_entropy(Matrix<float, 1, 10> out, Vector<float, 10> label) {
+	out = out.array();
+	label = label.array();
+
+	float sum = 0;
+
+	for (int i = 0; i < out.size(); i++) {
+		label[i] * log(out[i]);
 	}
 
+	return -1 * sum;
+}
+
+float relu(float x) {
+	return max(float(0.0), x);
+}
+
+Matrix<float, 1, 10> softmax(Matrix<float, 1, 10> out) {
+	out = out.array();
+	float sum = 0.0;
+
+	for (auto& x : out) {
+		sum = sum + exp(x);
+	}
+
+	for (int i = 0; i < out.size(); i++) {
+		out[i] = exp(out[i]) / sum;
+	}
+
+	out = out.matrix();
+
+	return out;
+}
+
+class Net {
+public:
 	void init_weights() {
 		//do this tomorrow i cba
 		//use kaiming initialization
 	}
 
-	Matrix<float, 1, 10> forward_pass(MatrixXd &data) {
+	Matrix<float, 1, 10> forward_pass(MatrixXf &data) {
 		data = data * W1;
 		data = data.rowwise() + b1.transpose();
 
-		data.unaryExpr(&relu);
+		data = data.unaryExpr(&relu);
 
 		data = data * W2;
 		data = data.rowwise() + b2.transpose();
-
-		data.unaryExpr(&relu);
+	
+		data = data.unaryExpr(&relu);
 
 		data = data * Out;
 		data = softmax(data);
@@ -31,42 +62,27 @@ public:
 		return data;
 	}
 
-	Matrix<float, 1, 10> softmax(Matrix<float, 1, 10> out) {
-		out = out.array();
-		double sum = 0.0;
-
-		for (auto& x : out) {
-			sum = sum + exp(x);
-		}
-
-		for (int i = 0; i < out.size(); i++) {
-			out[i] = exp(out[i]) / sum;
-		}
-
-		Matrix<float, 1, 10> result;
-		
-		for (auto& z : out) {
-			result << z;
-		}
-
-		return result;
-	}
-
-	double relu(double x) {
-		return max(0.0, x);
-	}
 private:
 	//input layer
 	Matrix<float, 1, 100> Input;
 
 	//first hidden layer
-	Matrix<float, 100, 64> W1;
-	Vector<float, 64> b1;
+	Matrix<float, 100, 64> W1 = Matrix<float, 100, 64>::Random();
+	Vector<float, 64> b1 = Vector<float, 64>::Random();
 
 	//second hidden layer
-	Matrix<float, 64, 64> W2;
-	Vector<float, 64> b2;
+	Matrix<float, 64, 64> W2 = Matrix<float, 64, 64>::Random();
+	Vector<float, 64> b2 = Vector<float, 64>::Random();
 
 	//output layer
-	Matrix<float, 64, 10> Out;
+	Matrix<float, 64, 10> Out = Matrix<float, 64, 10>::Random();
 };
+
+int main() {
+	Net net;
+
+	MatrixXf inp = MatrixXf::Random(1, 100);
+
+	Matrix<float, 1, 10> out = net.forward_pass(inp);
+
+}
